6012fbdd3afef24bea489d5bdf6bb9e381c96109
gorkavillara <gorkavillara@gmail.com>
2024-11-12 16:34:10 +0100

    Tema 3 - Debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
 import { useState } from "react"
 import "./App.css"
-// import ComponenteClaseCicloDeVida from "./components/ComponenteClaseCicloDeVida";
 import ComponenteFuncionalCicloDeVida from "./components/ComponenteFuncionalCicloDeVida"
-// import Contador from "./components/Contador";
-// import Saludo from "./components/Saludo";
-// import ComponenteClase from "./components/ComponenteClase";
-// import ComponenteFuncional, { ComponenteFuncional2 } from "./components/ComponenteFuncional";
+import { generaNombre } from "./lib/utils"
 
-// const miElemento = <p>Hola me llamo Gorka</p>;
+const muyLenta = () => {
+  const datos = 10000000
+  for (let i = 0; i<datos; i++) {
+    const a = 1
+  }
+}
+
+const funcionAsincrona = async () => {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res(true)
+    }, 2000)
+  })
+}
 
 function App() {
   const [visible, setVisible] = useState(false)
-  const toggleVisible = () => setVisible((vis) => !vis)
+  const [nombre, setNombre] = useState<string | null>(null)
+  const toggleVisible = async () => {
+    // const miConstante = { nombre: generaNombre() }
+    // setNombre(miConstante.nombre)
+    // console.time("funcionLenta")
+    // muyLenta()
+    // console.timeEnd("funcionLenta")
+    // setVisible((vis) => !vis)
+    await funcionAsincrona()
+    console.log(true)
+  }
   return (
     <div>
-      {/* <ComponenteClase />
-      <ComponenteFuncional />
-      <ComponenteFuncional2 /> */}
-      {/* <Saludo nombre="Gorka" color="red" />
-      <Saludo nombre="Albert" /> */}
-      {/* <Contador /> */}
-      {/* <ComponenteClaseCicloDeVida /> */}
       {visible ? <ComponenteFuncionalCicloDeVida /> : null}
-      {/* {visible ? (
-        <button onClick={toggleVisible}>Ocultar</button>
-      ) : (
-        <button onClick={toggleVisible}>Mostrar</button>
-      )} */}
-      <button onClick={toggleVisible}>{visible ? "Ocultar" : "Mostrar"}</button>
+      <button onClick={toggleVisible} id={nombre || "idgenerico"}>{visible ? "Ocultar" : "Mostrar"}</button>
+      {nombre && <p>Hola {nombre}</p>}
     </div>
   )
 }
---
+++ src/lib/utils.ts
@@ -0,0 +1,3 @@
+export const generaNombre = () => {
+    return "Josemi"
+}
