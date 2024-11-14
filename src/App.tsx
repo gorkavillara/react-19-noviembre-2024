302b3d3068f0f0e6636f0189accf5f6220d10b00
gorkavillara <gorkavillara@gmail.com>
2024-11-14 17:32:50 +0100

    prime react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import TypingTest from "./components/TypingTest"
+import { Button } from "primereact/button"
+import { InputText } from "primereact/inputtext"
+import { Menu } from "primereact/menu"
+import { useState } from "react"
+
+const items = [
+  { label: "New", icon: "pi pi-plus" },
+  { label: "Search", icon: "pi pi-search" },
+]
 
 const App = () => {
-  return <TypingTest />
+  const [value, setValue] = useState("")
+  return (
+    <div>
+      <h1>Hola mundo desde Prime</h1>
+      <Button>Haz clic</Button>
+      
+      <Menu model={items} />
+
+      <div className="card flex justify-content-center">
+        <InputText value={value} onChange={(e) => setValue(e.target.value)} />
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { PrimeReactProvider } from "primereact/api"
+// import "./index.css"
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import 'primeicons/primeicons.css';
+import './customPrime.css'
+        
+import App from "./App.tsx"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+  </StrictMode>
 )
