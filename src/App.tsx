2f14273ca9bf915ad33fcbd115d13fc8bbfef18e
gorkavillara <gorkavillara@gmail.com>
2024-11-14 18:11:46 +0100

    Tema 5 - Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import TypingTest from "./components/TypingTest"
+import { Typography } from "@mui/material"
+import Button from "@mui/material/Button"
 
 const App = () => {
-  return <TypingTest />
+  return (
+    <>
+      <Typography variant="h3" sx={{ py: 8 }}>Hola mundo desde Material UI</Typography>
+      <Button size="large" variant="contained">
+        Clic
+      </Button>
+      <Button size="medium" variant="outlined">
+        Clic
+      </Button>
+      <Button size="small" variant="text">
+        Clic
+      </Button>
+    </>
+  )
 }
 
 export default App
