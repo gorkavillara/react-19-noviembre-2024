976f473e6487228d537271e3a287a6225b28c57e
gorkavillara <gorkavillara@gmail.com>
2024-11-14 18:30:21 +0100

    Tema 5 - Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import TypingTest from "./components/TypingTest"
-
 const App = () => {
-  return <TypingTest />
+  return (
+    <div>
+      <h1 className="text-red-600 text-3xl font-semibold mb-4">
+        Hola mundo desde Tailwind
+      </h1>
+      <div className="flex space-x-4">
+        <button className={`bg-cyan-400 text-[#ffffff] py-4 px-8 rounded-lg hover:opacity-50 transition-all`}>
+          Botón 1
+        </button>
+        <button className="bg-cyan-400 text-white py-4 px-8 rounded-lg hover:opacity-50 transition-all">
+          Botón 2
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#1fb6ff",
+        pink: "#ff49db",
+        orange: "#ff7849",
+        green: "#13ce66",
+        "gray-dark": "#273444",
+        gray: "#8492a6",
+        "gray-light": "#d3dce6",
+      },
+    },
+  },
+  plugins: [],
+}
