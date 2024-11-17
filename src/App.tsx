e5749fedad9ee84750ca8e0064555c96e8fd6ff0
gorkavillara <gorkavillara@gmail.com>
2024-11-17 20:52:48 +0100

    Tema 06 - Listas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,62 @@
-import TypingTest from "./components/TypingTest"
+import { useState, useRef } from "react"
+import "./App.css"
+
+interface Alumno {
+  id: number | string
+  nombre: string
+  nota: number
+}
+
+const alumnos: Alumno[] = [
+  {
+    id: 1,
+    nombre: "Diego",
+    nota: 8,
+  },
+  {
+    id: 2,
+    nombre: "Antonio",
+    nota: 10,
+  },
+  {
+    id: 3,
+    nombre: "Adrián",
+    nota: 10,
+  },
+  {
+    id: 4,
+    nombre: "Albert",
+    nota: 8,
+  },
+]
+
+interface Todo {
+  id: number | string
+  text: string
+  completed: boolean
+}
 
 const App = () => {
-  return <TypingTest />
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [inputValue, setInputValue] = useState("")
+  const inputRef = useRef<HTMLInputElement>(null)
+  return (
+    <>
+      <h1>Ejercicios con listas</h1>
+      <input
+        type="text"
+        ref={inputRef}
+      />
+      <button>Añadir</button>
+      <ul>
+        {alumnos.map((alumno) => (
+          <li key={alumno.id}>
+            {alumno.id}. {alumno.nombre}: {alumno.nota}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default App
